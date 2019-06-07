45d608aed8a136bdfc4ea9a245d568c7c4764399
bouquety <abouquety1@gmail.com>
2019-06-07 16:33:40 +0200

    ahahahah

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -59009,7 +59009,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52486" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65417" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
